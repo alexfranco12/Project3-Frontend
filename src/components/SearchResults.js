8fb633aaa507e8cce99304c05ae62889dc3d15ea
amitmraval <amitmraval@yahoo.com>
2021-05-17 20:12:04 -0400

    fronend updates

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -5,7 +5,7 @@ import Map from "./Map";
 import "../map.css";
 import { Icon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/map-marker";
-import { Marker } from "google-maps-react";
+
 import location from "../images/location.png";
 
 function SearchResults(props) {
@@ -33,20 +33,6 @@ function SearchResults(props) {
           <Link to={`/${result.name}`}> {result.name}</Link>
         </div>
       ))}
-      <div className="google-map">
-        <Map location={{ lat: 34.0522, lng: -118.2437 }} zoomLevel={10}>
-          {results.map((result) => (
-            <Marker
-              position={{
-                lat: result.geometry.location.lat,
-                lng: result.geometry.location.lng,
-              }}
-              icon={location}
-              key={result.id}
-            />
-          ))}
-        </Map>
-      </div>
     </div>
   );
 }
